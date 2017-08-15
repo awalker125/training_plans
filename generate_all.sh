c708b037142e3788b43f9b3f2be515bb46543b89
Andrew Walker <awalker125@users.noreply.github.com>
2017-08-15 09:16:38 +0100

    refactor

--- generate_all.sh
+++ generate_all.sh
@@ -6,8 +6,31 @@ export THIS=$(basename $0)
 export RUNAS=$(whoami)
 export WHEREAMI=$(dirname $0)
 
+SUBDIR="${1}"
+WEEKS=6
 
-for YAML in $(ls ${WHEREAMI}/*.yaml)
+if [ -z "${2}" ]
+then
+	echo "creating default 6 week program"
+else
+	WEEKS=${2}
+	echo "creating ${WEEKS} week program"
+fi
+
+if [ -z "${SUBDIR}" ]
+then
+	echo "subdir not provided" && exit 99
+fi
+
+if [ -d "${SUBDIR}" ]
+then
+	echo "found subdir ${SUBDIR}"
+else
+	echo "could not find subdir ${SUBDIR}"
+fi
+
+
+for YAML in $(ls ${WHEREAMI}/${SUBDIR}/*.yaml)
 do
 
 	filename=$(basename $YAML)
@@ -16,12 +39,8 @@ do
 	
 	echo $filename
 	
-	#if [ -d ${WHEREAMI}/${filename} ]
-	#then
-	#	mv ${WHEREAMI}/${filename} ${WHEREAMI}/${filename}.${NOW}
-	#fi
+
 	
-	python ${WHEREAMI}/inol.py --output ${WHEREAMI}/${filename} --weeks 6  --config ${WHEREAMI}/${filename}.yaml
-	#python ${WHEREAMI}/inol.py --verbose --output ${WHEREAMI}/${filename} --weeks 6  --config ${WHEREAMI}/${filename}.yaml
+	python ${WHEREAMI}/inol.py --output ${WHEREAMI}/${SUBDIR}/${filename} --weeks ${WEEKS}  --config ${WHEREAMI}/${SUBDIR}/${filename}.yaml
 	
 done
